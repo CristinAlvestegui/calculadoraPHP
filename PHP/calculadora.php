92a2256eb3665e1a2d45d39908080df63487329d
Cristina Alvéstegui <104037804+CristinAlvestegui@users.noreply.github.com>
2023-01-20 16:51:41 -0300

    Update calculador PHP
    Adicionando mais exercicios 🏃🏼‍♀️

--- PHP/calculadora.php
+++ PHP/calculadora.php
@@ -21,6 +21,9 @@
     $nota2 = 7.1;
     $nota3 = 7.5;
     $cel = 31;
+    $A = 32;
+    $B = 23;
+    $C = 29;
     
     //Operações
     function soma($num1, $num2){
@@ -145,6 +148,10 @@
         return $den;
     }
 
+    function exerciQua(){
+
+    }
+
     function exerciCin(){
         $conver = 4*16**4 + 5*16**3 + 15*16**2 + 3*16**1 + 16*16**0;
         return $conver;
@@ -163,7 +170,6 @@
         $nota2 = 7.1;
         $nota3 = 7.5;
         $media = ($nota1 + $nota2 + $nota3) / 3;
-        
         return $media;
     }
 
@@ -173,15 +179,88 @@
         return $far;
     }
 
+    /*
     function exerciNove(){
-        $aniver = "05/11/1994"
+        $aniver = "05/11/1994" //definindo o aniversario
+        //vefiricar se essa data é valida, vamos ter que converter para int nossa string.
+        var_dump(checkdate($day, $mes, $year));
+        if(checkdate()== false){
+            return "Por favor digite uma data valida!";
+        } //Se for valida separar essa string depois do / e armazenar em cada uma das variaves.
+        $day = "";
+        $mes = "";
+        $year = 1994;
         
-        if($mes ==2){
+       
+        if(($year / 4 == 0) && ($year /400 == 0)){
+            $mes * 29;
+        }
+        if($mes == 02){
             $mes * 28;
         }else{
             $mes = $mes * 30;
         }
         $ano = $ano *365;
+    }
+
+    function anoBissex($year){
+        $year / 4 = $valor
+        $valor / 4 //ateee chegar em zero!
+
+        while($year >=0){
+            if(($year == 0) && ($mes == 02)){
+                $mes = 29;
+            }
+        }
+    }
+
+    */
+
+    function exerciDez(){
+
+    }
+
+    function exerciOnze(){
+        $aluno = "Luis";
+        $nota1 = 9.8;
+        $nota2 = 3.1;
+        $nota3 = 6.5;
+        $aritme = ($nota1 + $nota2 + $nota3)/ 3;
+
+        $peso1 = 5;
+        $peso2 = 2;
+        $peso3 = 4;
+
+        $ponde = (($peso1 * $nota1) + ($peso2 * $nota2) + ($peso3 * $nota3)) / $peso1 + $peso2 + $peso3;
+
+        $harmo = 3 / 1/$nota1 + 1/$nota2 + 1/$nota3;
+        
+        $msg = "A média Artimética é: $aritme <br> A média Ponderada é de: $ponde <br> E a média harmônica é de: $harmo"; 
+        return $msg;
+    }
+
+    function exerciDose(){
+        $valor = 
+        $base = $valor + $frete + $seguro + $outras
+    }
+
+    function exerciTreze(){
+        $A = 32;
+        $B = 23;
+        $C = 29;
+        $R =($A + $B)**2;
+        $S = ($B + $C)**2;
+        $D = ($R + $S) / 2;
+        return $D;
+    }
+
+    function exerciCator(){
+        $hF = 1.62;
+        $hM = 1.80; 
+        $mascu = (72.7*$hM) - 58;
+        $feme = (62.1 * $hF) - 44.7;
+        $msg = "<br>Altura femeninina declara foi de: $hF <br>Altura masculina declarada foi de: $hM <br> O peso ideal respectivamente seria de: $feme Kg, $mascu Kg";
+        return $msg;
     }
 
  
@@ -195,9 +274,11 @@
     echo "Percorrimos ".$km." km, e usamos ".$gas." litros. Em média foi um gasto de:".exerciUm($gas, $km);
     //echo pascal($pasc1);
     echo tabuada($num2)."<br>";
-    echo "O valor binario é:".$bin." Convertido para decimal é:".binEde()."<br>";
-    echo "hexadecimal:".$hexa." Convertido para decimal é:".exerciCin();
-    echo "Bem-vindo: ".$salesman."<br> Seu salário fixo é de: ".$sal."R$"."<br>O total de vendas desse mês foi de: ".$vendas."R$"."<br>Junto com a comissão fico um total de: ".exerciMeia()."R$";
+    echo "O valor binario é: $bin  Convertido para decimal é:".binEde()."<br>";
+    echo "hexadecimal: $hexa Convertido para decimal é:".exerciCin();
+    echo "Bem-vindo:  $salesman <br> Seu salário fixo é de:  $sal R$  <br>O total de vendas desse mês foi de:  $vendas R$  <br>Junto com a comissão fico um total de: ".exerciMeia()."R$";
     echo "<br>Oi ".$aluno." Bem-vindo ao Senac"."<br>Primeira nota: ".$nota1."<br>Segunda nota: ".$nota2."<br>Terceira nota:".$nota3."<br>A Média é de: ".exerciSete()."<br>";
-    echo "Temperatura em Celsius é de: ".$cel."°C "."<br>Convertendo para Fahrenheit é:".exerciOito()."°F";
-?>
+    echo "Temperatura em Celsius é de: ".$cel."°C "."<br>Convertendo para Fahrenheit é:".exerciOito()."°F <br>";
+    echo exerciOnze()."<br>";
+    echo "Valores inicias de A:$A, B:$B e C:$C <br> calculamos a expressão: D = R + S /2<br>Onde R = (A + B)**2 e S = (B + C)**2<br>Reasultado: ".exerciTreze()."<br>";
+    echo "Peso ideal".exerciCator();
